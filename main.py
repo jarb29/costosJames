e0d692f704b768f3a68a30dd75a1c4ec6101e002
jarb29 <jarb29@gmail.com>
2024-08-14 07:32:41 -0400

    new

--- main.py
+++ main.py
@@ -151,14 +151,3 @@ fig_futuro.update_traces(hovertemplate='PV=%{x}<br>total_kg=%{customdata[1]}<br>
 st.plotly_chart(fig_futuro)
 
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
