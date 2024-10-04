19ead1c9741ab0cd4d2002c47aedfde890836b6b
jarb29 <jarb29@gmail.com>
2024-10-04 15:53:44 -0300

    Add analytical functions and update dashboard visuals
    Added multiple utility functions in `util.py` for data processing, including date filtering, rounding, and month/year calculations. Enhanced `main.py` with updated UI elements, data sorting capabilities, and integrated widgets for better user interaction. Included a new logo.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from operator import itemgetter
 import plotly.express as px
 import streamlit as st
 import math
+import altair as alt
 
 
 dynamo = boto3.resource('dynamodb', region_name='us-east-1')
@@ -18,12 +19,36 @@ response_averages = table_averages.scan()
 response_futuro = table_futuro.scan()
 
 items = response['Items']
+
 items_averages = response_averages['Items']
 
 items_futuro = response_futuro['Items']
 
-precio_kg = st.sidebar.number_input('Enter value for precio_kg', value=360)
-precio_efectivo_minutos = st.sidebar.number_input('Enter value for precio_efectivo_minutos', value=210000)
+months, years, cm, cy = get_months_and_years_since("01/04/2024")
+st.set_page_config(
+    page_title="Kupfer Costos Nave1/Laser",
+    layout="centered",
+    page_icon="📉",
+
+    initial_sidebar_state="collapsed")
+alt.themes.enable("dark")
+
+
+with st.sidebar:
+    st.sidebar.image("data/logo.png", use_column_width=True)
+    st.title("📅 Nave1/Laser Costos")
+    default_month_index = months.index(cm)-1 # here to control the month
+    default_years_index = years.index(cy)
+    selected_month = st.sidebar.selectbox('Selecciones Mes', months, index=default_month_index)
+    selected_year = st.sidebar.selectbox('Seleccione Año', years, index=default_years_index)
+    precio_mes = st.sidebar.number_input('Enter valor for Costo Mes', value=10000000)
+    st.markdown("---")
+#######################
+# Dashboard Main Panel
+col = st.columns((2, 4, 4), gap='medium')
+
+precio_kg = st.sidebar.number_input('Enter valor for Costo por Kg', value=360)
+precio_efectivo_minutos = st.sidebar.number_input('Enter valor for Costo Hora/Corte', value=210000)
 
 
 while 'LastEvaluatedKey' in response_futuro:
@@ -51,80 +76,174 @@ grouped_data = {}
 for key, group in groupby(items, key=lambda x:x['pv']):
     grouped_data[key] = list(group)
 
-
 items_futuro.sort(key=itemgetter('pv'))
 
 grouped_data_futuro = {}
 for key, group in groupby(items_futuro, key=lambda x:x['pv']):
     grouped_data_futuro[key] = list(group)
 
-summed_data = sum_up_values(grouped_data)
+
+
+newest_dates = process_grouped_data(grouped_data)
+filtered_pvs = filter_by_year_month(newest_dates,selected_year, selected_month)
+
+
+grouped_data2 = {each: grouped_data[each] for each in filtered_pvs}
+
+summed_data = sum_up_values(grouped_data2)
 summed_data_futuro = sum_up_values(grouped_data_futuro)
 
 costos_data = calculate_total_price(summed_data, precio_kg, precio_efectivo_minutos/60)
 costos_data_futuro = calculate_total_price(summed_data_futuro, precio_kg, precio_efectivo_minutos/60)
 
 df = convert_dict_to_df(costos_data)
+
+kg_mes = round_to_two_decimals(sum(df['total_kg']))
+tiempo_mes = round_to_two_decimals(sum(df['total_tiempo_corte'])/60)
+costo_kg = round_to_two_decimals(sum(df['precio_kg']))
+costo_tiempo = round_to_two_decimals(sum(df['precio_tiempo']))
+
+deberia_kg = round_to_two_decimals(precio_mes/kg_mes)
+deberia_tiempo = round_to_two_decimals(precio_mes/tiempo_mes)
+
+
+
+st.title("Metrics Overview")
+st.markdown("---")
+
+# Custom CSS for colored metrics
+st.markdown(
+    """
+    <style>
+    .metric-container {
+        padding: 15px;
+        border-radius: 10px;
+        margin-bottom: 15px;
+        color: white;
+    }
+    .metric-label {
+        font-size: 16px;
+        font-weight: bold;
+    }
+    .metric-value {
+        font-size: 24px;
+        text-align: center;
+    }
+    .bg-total-kg {background-color: #4CAF50;}
+    .bg-total-time {background-color: #2196F3;}
+    .bg-cost-time {background-color: #FF9800;}
+    .bg-cost-kg {background-color: #9C27B0;}
+    .bg-deberia-kg {background-color: #F44336;}
+    .bg-deberia-time {background-color: #3F51B5;}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
+# Helper function to add a colored metric
+def colored_metric(label: str, value: str, css_class: str):
+    st.markdown(f"""
+        <div class="metric-container {css_class}">
+            <div class="metric-label">{label}</div>
+            <div class="metric-value">{value}</div>
+        </div>
+    """, unsafe_allow_html=True)
+
+
+# Monthly Metrics
+st.markdown("### Métricas del Mes")
+metric_cols1 = st.columns(2)
+with metric_cols1[0]:
+    colored_metric("Total KG", f"{kg_mes} kg", "bg-total-kg")
+with metric_cols1[1]:
+    colored_metric("Total Tiempo", f"{tiempo_mes} hours", "bg-total-time")
+st.markdown("---")
+
+# Current Prices
+st.markdown("### Precios Actuales")
+metric_cols2 = st.columns(2)
+with metric_cols2[0]:
+    colored_metric("Cost por Tiempo", f"${costo_tiempo}", "bg-cost-time")
+with metric_cols2[1]:
+    colored_metric("Cost por KG", f"${costo_kg}", "bg-cost-kg")
+st.markdown("---")
+
+# Ideal Prices
+st.markdown("### Precios Ideales")
+metric_cols3 = st.columns(2)
+with metric_cols3[0]:
+    colored_metric("Ideal por KG", f"${deberia_kg}", "bg-deberia-kg")
+with metric_cols3[1]:
+    colored_metric("Ideal por Tiempo", f"${deberia_tiempo}", "bg-deberia-time")
+
+
+
+st.markdown("---")
+
+# Calculate sum of 'total_kg'
+kg_mes = sum(df['total_kg'])
 df_futuro = convert_dict_to_df(costos_data_futuro)
+df_futuro_len = len(df_futuro)
 
+# Modify 'pv' and 'total_kg' columns
 df['pv'] = 'PV ' + df['pv']
-if len(df_futuro) > 0:
+df['total_kg'] = round(df['total_kg'] / 1000, 2)
+if df_futuro_len > 0:
     df_futuro['pv'] = 'PV ' + df_futuro['pv']
-df['total_kg'] = round(df['total_kg']/1000, 2)
-if len(df_futuro) > 0:
-    df_futuro['total_kg'] = round(df_futuro['total_kg']/1000, 2)
+    df_futuro['total_kg'] = round(df_futuro['total_kg'] / 1000, 2)
 
-# Create a button for sorting
+# Sidebar Sorting Buttons
 col1, col2 = st.sidebar.columns(2)
 
-kg_sort = col1.button('Ordenar precio_kg')
+kg_sort = col1.button('Ordenar Por Costo/Kg')
 if kg_sort:
     df = df.sort_values(by='precio_kg', ascending=False)
-    if len(df_futuro) > 0:
+    if df_futuro_len > 0:
         df_futuro = df_futuro.sort_values(by='precio_kg', ascending=False)
 
-tiempo_sort = col2.button('Ordenar precio_tiempo')
+tiempo_sort = col2.button('Ordenar Costo/Tiempo')
 if tiempo_sort:
     df = df.sort_values(by='precio_tiempo', ascending=False)
-    if len(df_futuro) > 0:
+    if df_futuro_len > 0:
         df_futuro = df_futuro.sort_values(by='precio_tiempo', ascending=False)
 
-
-
-
+# Rename columns for better readability
 df = df.rename(columns={'precio_kg': 'Costo Kg', 'precio_tiempo': 'Costo Tiempo'})
+
+# Plot chunks of the DataFrame
 total_plots = math.ceil(len(df) / 30)
 for i in range(total_plots):
-
     df_chunk = df[i * 30: (i + 1) * 30]
     custom_data = df_chunk.values.T.tolist()
-    fig = px.bar(df_chunk, x='pv', y=['Costo Kg','Costo Tiempo'],
-                 title='Costo KG and Costo Tiempo for each PV: PASADO',
-                 labels={'value': 'Costo', 'variable': 'Tipo', 'pv': 'PV'},
-                 height=800, custom_data=custom_data)  # Increased plot height
 
-
+    fig = px.bar(
+        df_chunk, x='pv', y=['Costo Kg', 'Costo Tiempo'],
+        title='Costo KG and Costo Tiempo for each PV: PASADO',
+        labels={'value': 'Costo', 'variable': 'Tipo', 'pv': 'PV'},
+        height=800, custom_data=custom_data
+    )  # Increased plot height
 
     # Adjust x-axis labels and set barmode as 'group'
     fig.update_layout(
         title={
-            'text': "PASADO<br><sub>Costo KG y Costo Tiempo por cada PV<sub>",
+            'text': "Pv Procesadas<br><sub>Costo KG y Costo Tiempo por cada PV<sub>",
             'y': 0.9,
             'x': 0.5,
             'xanchor': 'center',
             'yanchor': 'top',
             'font': {'size': 20, 'color': 'black', 'family': 'Arial, bold'},
         },
-        autosize=True,   # This line disables the autoresize
+        autosize=True,  # This line disables the autoresize
         xaxis_tickangle=-45,
         barmode='group',
         xaxis=dict(
-            tickfont=dict(size=14), # Increase font size for x-axis values
-            title_font=dict(size=14) # Increase font size for x-axis label
+            tickfont=dict(size=14),  # Increase font size for x-axis values
+            title_font=dict(size=14)  # Increase font size for x-axis label
         ),
         yaxis=dict(
-            tickfont=dict(size=14), # Increase font size for y-axis values
-            title_font=dict(size=14) # Increase font size for y-axis label
+            tickfont=dict(size=14),  # Increase font size for y-axis values
+            title_font=dict(size=14)  # Increase font size for y-axis label
         ),
     )
 
@@ -139,34 +258,36 @@ for i in range(total_plots):
 
 st.markdown("---")
 if len(df_futuro) > 0:
-    custom_data_futuro=df_futuro.values.T.tolist()
+    custom_data_futuro = df_futuro.values.T.tolist()
     df_futuro = df_futuro.rename(columns={'precio_kg': 'Costo Kg', 'precio_tiempo': 'Costo Tiempo'})
 
-    fig_futuro = px.bar(df_futuro, x='pv', y=['Costo Kg','Costo Tiempo'],
-                 title='Costo KG y Costo Tiempo por cada PV: FUTURO',
-                 labels={'value': 'Costo', 'variable': 'Tipo', 'pv': 'PV'},
-                 height=800, custom_data=custom_data_futuro)  # Increased plot height
+    fig_futuro = px.bar(
+        df_futuro, x='pv', y=['Costo Kg', 'Costo Tiempo'],
+        title='Costo KG y Costo Tiempo por cada PV: FUTURO',
+        labels={'value': 'Costo', 'variable': 'Tipo', 'pv': 'PV'},
+        height=800, custom_data=custom_data_futuro
+    )  # Increased plot height
 
     # Adjust x-axis labels and set barmode as 'group'
     fig_futuro.update_layout(
         title={
-            'text': "FUTURO<br><sub>Costo KG y Costo Tiempo por cada PV<sub>",
+            'text': "Pv en Proceso<br><sub>Costo KG y Costo Tiempo por cada PV<sub>",
             'y': 0.9,
             'x': 0.5,
             'xanchor': 'center',
             'yanchor': 'top',
-            'font': {'size': 20, 'color': 'black', 'family': 'Arial, bold'},
+            'font': {'size': 20, 'color': 'black', 'family': 'Arial, bold'}
         },
-        autosize=True,   # This line disables the autoresize
+        autosize=True,  # This line disables the autoresize
         xaxis_tickangle=-90,
         barmode='group',
         xaxis=dict(
-            tickfont=dict(size=14), # Increase font size for x-axis values
-            title_font=dict(size=14) # Increase font size for x-axis label
+            tickfont=dict(size=14),  # Increase font size for x-axis values
+            title_font=dict(size=14)  # Increase font size for x-axis label
         ),
         yaxis=dict(
-            tickfont=dict(size=14), # Increase font size for y-axis values
-            title_font=dict(size=14) # Increase font size for y-axis label
+            tickfont=dict(size=14),  # Increase font size for y-axis values
+            title_font=dict(size=14)  # Increase font size for y-axis label
         ),
     )
 
@@ -176,19 +297,18 @@ if len(df_futuro) > 0:
                                            'Costo KG = %{customdata[3]:,.0f} Pesos<br>'
                                            'Costo Tiempo = %{customdata[4]:,.0f} Pesos')
     st.plotly_chart(fig_futuro)
-
     st.markdown("---")
 
-
-fig_averages = px.bar(df_average, x='espesor',
-                      y='velocidad',
-                     color_discrete_sequence=px.colors.qualitative.Plotly, # let's add this
-                     title='Espesor Vs Velocidad')  # Increased plot height
+# Plotting Averages Data
+fig_averages = px.bar(
+    df_average, x='espesor', y='velocidad',
+    color_discrete_sequence=px.colors.qualitative.Plotly,  # let's add this
+    title='Espesor Vs Velocidad'
+)  # Increased plot height
 
 # Adjust x-axis labels and set barmode as 'group'
 fig_averages.update_layout(
     autosize=True,
-
 )
 
 st.plotly_chart(fig_averages)
--- util.py
+++ util.py
@@ -63,3 +63,129 @@ def convert_dict_to_df(data):
 
     return df
 
+from datetime import datetime
+def get_months_and_years_since(date_str):
+
+    initial_date = datetime.strptime(date_str, "%d/%m/%Y")
+    current_date = datetime.now()
+
+    months = set()
+    years = set()
+
+    while initial_date <= current_date:
+        months.add(initial_date.month)
+        years.add(initial_date.year)
+        initial_date = add_months(initial_date, 1)
+
+    # Separate current month and year
+    cur_month = current_date.month
+    cur_year = current_date.year
+
+    return sorted(list(months)), sorted(list(years)), cur_month, cur_year
+
+
+def add_months(date, months):
+    month = date.month - 1 + months
+    year = date.year + month // 12
+    month = month % 12 + 1
+    day = min(date.day, [31,29,31,30,31,30,31,31,30,31,30,31][month-1])
+    return datetime(year, month, day)
+
+
+
+from datetime import datetime
+from typing import List, Dict, Any
+
+
+def process_grouped_data(grouped_data: Dict[str, List[Dict[str, Any]]]) -> List[Dict[str, str]]:
+    """
+    Process the grouped data to extract the newest 'closeAt' date for each 'pv' in YYYY-MM-DD format.
+
+    Args:
+    - grouped_data: A dictionary where each key is a 'pv' and the value is a list of dictionaries containing 'closeAt' dates.
+
+    Returns:
+    - A list of dictionaries containing 'pv' and the newest 'closeAt' date in YYYY-MM-DD format.
+    """
+
+    def extract_date_ymd(timestamp: str) -> str:
+        """
+        Extract the date in YYYY-MM-DD format from an ISO 8601-formatted timestamp string.
+
+        Args:
+        - timestamp: An ISO 8601-formatted timestamp string.
+
+        Returns:
+        - The date as a string in YYYY-MM-DD format.
+        """
+        if timestamp.endswith('Z'):
+            timestamp = timestamp[:-1]
+        datetime_obj = datetime.fromisoformat(timestamp)
+        return datetime_obj.strftime('%Y-%m-%d')
+
+    def find_newest_date(dates: List[str]) -> str:
+        """
+        Find the newest date from a list of ISO 8601-formatted date strings.
+
+        Args:
+        - dates: A list of ISO 8601-formatted date strings.
+
+        Returns:
+        - The newest date as an ISO 8601-formatted date string.
+        """
+        datetime_dates = [datetime.fromisoformat(date.replace('Z', '+00:00')) for date in dates]
+        newest_date = max(datetime_dates)
+        return newest_date.isoformat() + 'Z'
+
+    results = []
+    for pv, data in grouped_data.items():
+        closeAt_dates = [item['closeAt'] for item in data if 'closeAt' in item]
+        if closeAt_dates:
+            newest_date = find_newest_date(closeAt_dates)
+            results.append({'pv': pv, 'closeAt': extract_date_ymd(newest_date)})
+
+    return results
+
+from typing import List, Dict, Any
+
+
+def filter_by_year_month(data: List[Dict[str, Any]], year: int, month: int) -> List[str]:
+    """
+    Filter the list of pv based on the given year and month.
+
+    Args:
+    - data: A list of dictionaries where each dictionary contains 'pv' and 'closeAt' keys.
+    - year: The year to filter by.
+    - month: The month to filter by.
+
+    Returns:
+    - A list of 'pv' strings that match the given year and month.
+    """
+    filtered_pvs = []
+    for item in data:
+        close_at_date = item.get('closeAt')
+        if close_at_date:
+            date_parts = close_at_date.split('-')
+            item_year = int(date_parts[0])
+            item_month = int(date_parts[1])
+
+            if item_year == year and item_month == month:
+                filtered_pvs.append(item['pv'])
+
+    return filtered_pvs
+
+def round_to_two_decimals(value: float) -> float:
+    """
+    Rounds a floating-point number to two decimal places.
+
+    Args:
+    - value: The floating-point number to round.
+
+    Returns:
+    - The number rounded to two decimal places.
+    """
+    return round(value, 2)
+
+
+
+
